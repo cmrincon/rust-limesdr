2a87f481171401a82b4162b6431b44334f7ffe58
cmrincon <cmrincon611@hotmail.com>
2023-02-06 10:22:20 +0000

    Changed lib directory path

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@
 
 fn main() {
 
-    println!("cargo:rustc-link-search=/home/cmri/programming/pw1/lib");
+    println!("cargo:rustc-link-search=/home/cmri/programming/rust-limesdr/lib");
     println!("cargo:rustc-link-lib=dylib=LimeSuite");
     println!("cargo:rustc-link-lib=dylib=pthread");
     println!("cargo:rustc-link-lib=dylib=usb-1.0");
